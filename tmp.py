ef2e9e99b1ee8426fda98a9a077189a088c66ae6
KiStroh <128726312+KiStroh@users.noreply.github.com>
2023-05-18 19:10:45 +0000

    Attempted the first section - badly

--- tmp.py
+++ tmp.py
@@ -1,21 +1,17 @@
-# import folium
-# from io import BytesIO
-# import matplotlib.pyplot as plt
-# import matplotlib.dates as dates
-# import os
-# import pathlib
-# import requests
-# import subprocess
-
-# BEGIN SOLUTION
-import os
-import pathlib
-import subprocess
 from io import BytesIO
 
 import folium
+
+import os 
+
 import matplotlib.pyplot as plt
+
 import matplotlib.dates as dates
+
 import pandas as pd
+
+import pathlib
+
 import requests
-# END SOLUTION
+
+import subprocess
