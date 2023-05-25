3ff55fd971d95042103c507af966c9756e3fbc1d
KiStroh <128726312+KiStroh@users.noreply.github.com>
2023-05-25 00:46:51 +0000

    Got past the first few!

--- tmp.py
+++ tmp.py
@@ -1,17 +1,17 @@
+import os
+import pathlib
+import subprocess
 from io import BytesIO
 
+# Third Party?
 import folium
-
-import os 
-
-import matplotlib.pyplot as plt
-
 import matplotlib.dates as dates
-
+import matplotlib.pyplot as plt
 import pandas as pd
-
-import pathlib
-
 import requests
 
-import subprocess
+m = folium.Map(location=[44.08109849, -102.4012746])
+
+m
+
+# raise NotImplementedError("Map of Wasta, SD")
