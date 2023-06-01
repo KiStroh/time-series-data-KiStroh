54564701e5c4aac515187da68944862656b06674
KiStroh <128726312+KiStroh@users.noreply.github.com>
2023-06-01 20:13:35 +0000

    Finishing Downloading Data

--- tmp.py
+++ tmp.py
@@ -1,17 +1,2 @@
-import os
-import pathlib
-import subprocess
-from io import BytesIO
-
-# Third Party?
-import folium
-import matplotlib.dates as dates
-import matplotlib.pyplot as plt
-import pandas as pd
-import requests
-
-m = folium.Map(location=[44.08109849, -102.4012746])
-
-m
-
-# raise NotImplementedError("Map of Wasta, SD")
+for i, line in enumerate(Req.content.splitlines()[:10]):
+    print(i, line)
